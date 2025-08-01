0a437a10c9e1885a8963fed2bd0e6c1358c70b95
raven-panda <ambroise.maire7@gmail.com>
2025-08-01 12:46:53 +0200

    chore(config): added swagger ui, replaces openapi

--- Program.cs
+++ Program.cs
@@ -12,7 +12,8 @@ builder.Services.AddScoped<IStudentTaskRepository, StudentTaskRepositoryImp>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UsePathBase("/api/v1");
@@ -26,7 +27,12 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = "swagger";
+    });
 }
 
 app.UseHttpsRedirection();
