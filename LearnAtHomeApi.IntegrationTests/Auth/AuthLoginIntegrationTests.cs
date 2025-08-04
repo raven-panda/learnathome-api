66a83056f111792dbb62530dda2166b012445d1e
raven-panda <ambroise.maire7@gmail.com>
2025-08-04 15:02:16 +0200

    test(auth/user login): implemented tests for user login

---
+++ LearnAtHomeApi.IntegrationTests/Auth/AuthLoginIntegrationTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using LearnAtHomeApi.Authentication.Dto;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LearnAtHomeApi.FunctionalTests.Auth;
+
+public class AuthLoginIntegrationTests
+    : IClassFixture<LearnAtHomeWebApplicationFactory>,
+        IDisposable
+{
+    private readonly CookieContainer _cookieContainer = new();
+    private readonly HttpClient _client;
+    private readonly LearnAtHomeWebApplicationFactory _factory;
+    private readonly AuthRegisterDto _registerDto = new()
+    {
+        Email = "test@test.com",
+        Username = "TestUser",
+        Password = "Password70*$",
+        PasswordConfirm = "Password70*$",
+    };
+
+    public AuthLoginIntegrationTests(LearnAtHomeWebApplicationFactory factory)
+    {
+        _factory = factory;
+
+        _client = factory.CreateClient();
+    }
+
+    private async Task BeforeEachTest()
+    {
+        await _client.PostAsJsonAsync("/api/v1/auth/register", _registerDto);
+    }
+
+    /// <summary>
+    /// When I login with valid dto, API should return 200 success with the access_token and refresh_token cookies
+    /// </summary>
+    [Fact]
+    public async Task LoginValid_Returns200AndTokens()
+    {
+        await BeforeEachTest();
+
+        var loginDto = new AuthLoginDto
+        {
+            Email = _registerDto.Email,
+            Password = _registerDto.Password,
+        };
+        await LoginUser(
+            loginDto,
+            HttpStatusCode.OK,
+            response =>
+            {
+                var setCookieHeader = response.Headers.GetValues("Set-Cookie").ToList();
+                Assert.Contains(setCookieHeader, c => c.StartsWith("refresh_token="));
+                Assert.Contains(setCookieHeader, c => c.StartsWith("session_token="));
+            }
+        );
+    }
+
+    /// <summary>
+    /// When I login with valid dto, API should return 401 Unauthorized
+    /// </summary>
+    [Fact]
+    public async Task LoginInvalidEmail_Returns401()
+    {
+        await BeforeEachTest();
+
+        var loginDto = new AuthLoginDto
+        {
+            Email = _registerDto.Email + "_invalidString",
+            Password = _registerDto.Password,
+        };
+        await LoginUser(loginDto, HttpStatusCode.Unauthorized);
+    }
+
+    /// <summary>
+    /// When I login with valid dto, API should return 401 Unauthorized
+    /// </summary>
+    [Fact]
+    public async Task LoginInvalidPassword_Returns401()
+    {
+        await BeforeEachTest();
+
+        var loginDto = new AuthLoginDto
+        {
+            Email = _registerDto.Email,
+            Password = _registerDto.Password + "_invalidString",
+        };
+        await LoginUser(loginDto, HttpStatusCode.Unauthorized);
+    }
+
+    private async Task LoginUser(
+        AuthLoginDto dto,
+        HttpStatusCode expectedStatusCode,
+        Action<HttpResponseMessage>? responseAction = null
+    )
+    {
+        var response = await _client.PostAsJsonAsync("/api/v1/auth/login", dto);
+        Assert.Equal(expectedStatusCode, response.StatusCode);
+
+        responseAction?.Invoke(response);
+    }
+
+    public void Dispose()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.EnsureDeleted();
+    }
+}
