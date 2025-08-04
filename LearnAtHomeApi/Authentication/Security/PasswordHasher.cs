dbef44c24e7edf26fc3cba9b67db18caf9a945a8
raven-panda <ambroise.maire7@gmail.com>
2025-08-04 18:49:11 +0200

    docs: added comments to PasswordHasher class

--- LearnAtHomeApi/Authentication/Security/PasswordHasher.cs
+++ LearnAtHomeApi/Authentication/Security/PasswordHasher.cs
@@ -13,6 +13,9 @@ public interface IPasswordHasher
 
 internal sealed class PasswordHasher : IPasswordHasher
 {
+    /// <summary>
+    /// Constants defined for V1 tokens
+    /// </summary>
     private class V1Constants
     {
         internal const string VersionIdentifier = "V1";
@@ -23,6 +26,11 @@ internal sealed class PasswordHasher : IPasswordHasher
 
     private readonly HashAlgorithmName AlgorithmV1 = HashAlgorithmName.SHA512;
 
+    /// <summary>
+    /// Hash a string using the last version hasher constants
+    /// </summary>
+    /// <param name="password">String to hash</param>
+    /// <returns>Hashed string (password and salt)</returns>
     public string Hash(string password)
     {
         byte[] salt = RandomNumberGenerator.GetBytes(V1Constants.SaltSize);
@@ -37,6 +45,16 @@ internal sealed class PasswordHasher : IPasswordHasher
         return $"{Convert.ToBase64String(hash)}-{Convert.ToBase64String(salt)}-{Convert.ToBase64String(Encoding.ASCII.GetBytes(V1Constants.VersionIdentifier))}";
     }
 
+    /// <summary>
+    /// Compares a hashed string with the given string.
+    /// <br /> <br/>
+    /// It points to other methods for each hasher version.
+    /// <br />
+    /// This is to ensure that if we use a new hash system, older passwords will still be parseable.
+    /// </summary>
+    /// <param name="hashedPassword">Already hashed string (password and salt)</param>
+    /// <param name="providedPassword">String to hash and compare</param>
+    /// <returns>True if the comparison was a success</returns>
     public bool VerifyHashedPassword(string hashedPassword, string providedPassword)
     {
         string[] splitHash = hashedPassword.Split('-');
@@ -57,7 +75,17 @@ internal sealed class PasswordHasher : IPasswordHasher
         return false;
     }
 
-    public bool VerifyHashedPasswordV1(
+    /// <summary>
+    /// Compares a hashed string with the given string using V1 hasher constants.
+    /// </summary>
+    /// <param name="hash">Already hashed password</param>
+    /// <param name="salt">Salt used to hash the password</param>
+    /// <param name="providedPassword">Given string to compare with</param>
+    /// <param name="hashSize">Size of the original hash</param>
+    /// <param name="iterations">Number of iterations of the original hash</param>
+    /// <param name="algorithm">Algorythm used for the original hash</param>
+    /// <returns>True if the comparison was a success</returns>
+    private bool VerifyHashedPasswordV1(
         byte[] hash,
         byte[] salt,
         string providedPassword,
