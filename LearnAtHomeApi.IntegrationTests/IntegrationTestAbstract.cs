5097e1df947ad73dae521e27ff9ebc63c5a3c161
raven-panda <ambroise.maire7@gmail.com>
2025-08-04 15:56:25 +0200

    feat(tests): created test abstract class

---
+++ LearnAtHomeApi.IntegrationTests/IntegrationTestAbstract.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LearnAtHomeApi.FunctionalTests;
+
+public class IntegrationTestAbstract(LearnAtHomeWebApplicationFactory factory)
+    : IClassFixture<LearnAtHomeWebApplicationFactory>,
+        IAsyncLifetime
+{
+    protected readonly HttpClient Client = factory.CreateClient();
+
+    public async Task InitializeAsync()
+    {
+        await Task.Delay(50);
+    }
+
+    public Task DisposeAsync()
+    {
+        using var scope = factory.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        db.Database.EnsureDeleted();
+        return Task.CompletedTask;
+    }
+}
